dcc84d6c1e11e057446b297d64915c46e03e50b3
Bogdan <bogdan.ciuca.dev@gmail.com>
2023-07-11 23:01:47 +0300

    Remove node_modules, add vite, fix imports

--- esbuild.config.js
+++
@@ -1,7 +0,0 @@
-require('esbuild').build({
-    entryPoints: ['src/index.js'],
-    outfile: 'dist/bundle.js',
-    bundle: true,
-    target: 'web',
-    sourcemap: 'inline',
-  }).catch(() => process.exit(1))
